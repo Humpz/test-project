557062ae91f178142a6df0af645e917467adf527
Humphrey Aljas <aljashumphrey@gmail.com>
2021-11-09 02:54:07 +0800

    Installed Laravel, Vue JS, Vue Router, Tailwind CSS

---
+++ resources/js/components/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p class="text-xs text-gray-500">Test Sample</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "app.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
